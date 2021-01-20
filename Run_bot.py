46a65359a829d0c07ec28beb48e645c87f2ce21c
José Vítor <jose.vitor.sem.c@gmail.com>
2021-01-19 22:09:15 -0300

    Name changes

--- Run_bot.py
+++ Run_bot.py
@@ -13,27 +13,27 @@ async def on_ready():
 @client.command()
 @commands.has_permissions(administrator=True)
 async def load(ctx, extension):
-    client.load_extension(f'Engrenagens.{extension}')
+    client.load_extension(f'Cogs.{extension}')
     await ctx.send(f"Cog {extension} loaded")
 
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def unload(ctx, extension):
-    client.unload_extension(f'Engrenagens.{extension}')
+    client.unload_extension(f'Cogs.{extension}')
     await ctx.send(f"Cog {extension} unloaded")
 
 
 @client.command()
 @commands.has_permissions(administrator=True)
 async def reload(ctx, extension):
-    client.unload_extension(f'Engrenagens.{extension}')
-    client.load_extension(f'Engrenagens.{extension}')
+    client.unload_extension(f'Cogs.{extension}')
+    client.load_extension(f'Cogs.{extension}')
     await ctx.send(f"Cog {extension} reloaded")
 
 
-for file in os.listdir("./Engrenagens"):
+for file in os.listdir("./Cogs"):
     if file.endswith(".py"):
-        client.load_extension(f'Engrenagens.{file[:-3]}')
+        client.load_extension(f'Cogs.{file[:-3]}')
 
 client.run(os.environ["TOKEN"])
